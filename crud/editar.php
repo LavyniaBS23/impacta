43bfe55a857ba21caf0e94e7919015ab9a862deb
lavynia <lavyniabarbosa317@gmail.com>
2022-03-17 16:52:33 -0300

    update finalizado

--- crud/editar.php
+++ crud/editar.php
@@ -16,6 +16,7 @@ if(isset($_GET['id'])){
     <div class = "col s12 m6 push-m3">
         <h3 class= "ligth">Editar cliente</h3>
         <form action="php_action/update.php" method="POST">
+            <input type = "hidden" name = 'id' value = "<?php echo $dados['id'];?>">
             <div class="input-field col s12">
                <input type="text" name="nome" id ="nome" value="<?php echo $dados['nome'];?>">
                <label for="nome">Nome</label> 
@@ -32,7 +33,7 @@ if(isset($_GET['id'])){
                <input type="text" name="idade" id ="idade" value="<?php echo $dados['idade']; ?>">
                <label for="idade">Idade</label> 
             </div>
-            <button type="submit" name = "btn-cadastrar" class="btn">ATUALIZAR</button>
+            <button type="submit" name = "btn-editar" class="btn">ATUALIZAR</button>
             <a href="index.php" class="btn green" >lista de clientes</a>
         </form>
     </div>
